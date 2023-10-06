86ae5413940873d813d56a157bac06cb2bcfd263
Fredrick Makoffu <fredrick@solutech.co.ke>
2023-10-06 16:22:38 +0300

    Updates to Home page

---
+++ src/components/Home/Banner.svelte
@@ -0,0 +1,32 @@
+<div id="banner">
+    BUILD BLESS TEST DEPLOY BUG BUILD TEST BUILD TEST DEPLOY WRITE WAIT ERASE BLESS DRAFT WAIT BLESS TEST DEPLOY SING STOP AGONIZE COMPOSE SING STOP AGONIZE WRITE EVANGELIZE
+</div>
+
+<div id="overlay"></div>
+
+<style lang="scss" scoped>
+    $text-color: #333;
+
+    #banner {
+        height: -webkit-fill-available;
+        width: -webkit-fill-available;
+        position: absolute;
+        font-family: "Clash Grotesk", sans-serif;
+        -webkit-text-stroke: 0.5px #333;
+        background-color: #eee;
+        color: transparent;
+        font-size: 10rem;
+        overflow: hidden;
+        z-index: 1; 
+        line-height: 9rem;
+        opacity: 0.5;
+    }
+
+    #overlay {
+        position: absolute;
+        z-index: 1;
+        width: -webkit-fill-available;
+        height: -webkit-fill-available;
+        background: url('../../assets/images/bedge-grunge.png');
+    }
+</style>
